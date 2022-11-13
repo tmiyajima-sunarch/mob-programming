02b57097f0184dae2ad91e413461d8f7e1f4ed0d
tmiyajima-sunarch <47751609+tmiyajima-sunarch@users.noreply.github.com>
2022-11-13 09:19:37 +0000

    パスワードジェネレーターのベース実装

---
+++ password-generator/src/main/java/com/example/mob/programming/passwordgenerator/PasswordGenerator.java
@@ -0,0 +1,13 @@
+package com.example.mob.programming.passwordgenerator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordGenerator {
+  
+  public String generate(int length, boolean useNumber, boolean useSign) {
+    // TODO: ここを実装する
+    return "TODO";
+  }
+
+}
--- password-generator/src/main/java/com/example/mob/programming/passwordgenerator/PasswordGeneratorApplication.java
+++ password-generator/src/main/java/com/example/mob/programming/passwordgenerator/PasswordGeneratorApplication.java
@@ -2,10 +2,40 @@ package com.example.mob.programming.passwordgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PasswordGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(PasswordGeneratorApplication.class, args);
+    }
+
+    @Controller
+    public static class IndexController {
+        @GetMapping("/")
+        String index() {
+            return "index";
+        }
+    }
+
+    @RestController
+    public static class GenerateController {
+        private final PasswordGenerator passwordGenerator;
+
+        public GenerateController(PasswordGenerator passwordGenerator) {
+            this.passwordGenerator = passwordGenerator;
+        }
+
+        @PostMapping("/generate")
+        String generate(@RequestBody GenerateInput input) {
+            return this.passwordGenerator.generate(input.length(), input.useNumber(), input.useSign());
+        }
+    }
+
+    public record GenerateInput(Integer length, Boolean useNumber, Boolean useSign) {
     }
 }
